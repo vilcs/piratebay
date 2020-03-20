3db49a10993eda6720e5f44376f97f842ffbc49e
vilcs <mikymogul@hotmail.com>
2020-03-19 21:01:26 -0400

    piratebay version 0.0.1

---
+++ src/main/java/com/miguel/piratebay/PiratebayApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.piratebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratebayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratebayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/piratebay/PiratebayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.piratebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratebayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
